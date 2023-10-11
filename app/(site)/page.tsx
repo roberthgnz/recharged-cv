65ce20e58d1a008f73dc01ecf71d521b392cc4d0
Roberth González <reliutg@gmail.com>
2023-10-11 14:47:44 +0200

    aaa

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,12 +1,18 @@
 import { redirect } from "next/navigation"
+
 import { getServerSession } from "@/utils/auth"
 
-import { Hero } from "@/components/Hero"
+// import { Hero } from "@/components/Hero"
 
 export default async function Home() {
   const session = await getServerSession()
 
   if (session) redirect("/resumes")
 
-  return <Hero />
+  return (
+    <>
+      <h1>WIP</h1>
+      <p>Work in progress</p>
+    </>
+  )
 }
