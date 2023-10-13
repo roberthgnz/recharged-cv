9b61cac7a90d8fc28ad84721069d4a93e6af00d7
Roberth González <reliutg@gmail.com>
2023-10-13 17:27:05 +0200

    fix: add auth callback

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,7 +22,7 @@ export const Hero = () => {
     const { error } = await supabase.auth.signInWithOtp({
       email,
       options: {
-        emailRedirectTo: "https://rcv.digital/resumes",
+        emailRedirectTo: "https://rcv.digital/auth/callback",
       },
     })
 
