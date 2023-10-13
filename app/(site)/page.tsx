64a9509ed40d03ab43764e31f5d8caf4fe96a382
Roberth González <reliutg@gmail.com>
2023-10-13 17:22:08 +0200

    feat: add login button in home page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,18 +1,32 @@
+import { cookies } from "next/headers"
+import Link from "next/link"
 import { redirect } from "next/navigation"
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
 
-import { getServerSession } from "@/utils/auth"
+import { Button } from "@/components/ui/button"
 
 export const dynamic = "force-dynamic"
 
 export default async function Home() {
-  const session = await getServerSession()
+  const cookieStore = cookies()
 
-  if (session) redirect("/resumes")
+  const supabase = createServerComponentClient({
+    cookies: () => cookieStore,
+  })
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  if (user) redirect("/resumes")
 
   return (
     <div className="space-y-4 text-center">
       <h1>WIP</h1>
       <p>Work in progress</p>
+      <Button asChild>
+        <Link href={"/login"}>Sign In</Link>
+      </Button>
     </div>
   )
 }
